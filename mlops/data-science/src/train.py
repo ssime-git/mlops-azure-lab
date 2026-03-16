6b8467c32c700bd1da7ab02d3a9b08ad29d3d94f
ssime-git <sebastien.s@datascientest.com>
2026-03-16 14:38:59 +0100

    feat: max_iter 300

--- mlops/data-science/src/train.py
+++ mlops/data-science/src/train.py
@@ -32,5 +32,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_dir", default="data/processed")
     parser.add_argument("--model_dir", default="outputs/model")
-    parser.add_argument("--max_iter", type=int, default=200)
+    parser.add_argument("--max_iter", type=int, default=300)
     main(parser.parse_args())
